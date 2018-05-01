0abcd0e72d039d2794d13b85880f426d9ffcac7b
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-01 17:12:08 +0000

    Refactor pg search

--- test_script.js
+++ test_script.js
@@ -12,25 +12,32 @@ const client = new pg.Client({
 
 const query = `SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1`;
 
+[, , name] = process.argv;
+
 client.connect(err => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query(query, [process.argv[2]], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log("Searching ...");
-    console.log(
-      `Found ${result.rows.length} person(s) by the name '${process.argv[2]}':`
-    );
-    for (const [index, row] of result.rows.entries()) {
+  client
+    .query(query, [name])
+    .then(result => {
+      console.log("Searching ...");
       console.log(
-        `- ${index + 1}: ${row.first_name} ${
-          row.last_name
-        }, born '${row.birthdate.toISOString().slice(0, 10)}'`
+        `Found ${result.rows.length} person(s) by the name '${
+          process.argv[2]
+        }':`
       );
-    }
-    client.end();
-  });
+      for (const [index, row] of result.rows.entries()) {
+        console.log(
+          `- ${index + 1}: ${row.first_name} ${
+            row.last_name
+          }, born '${row.birthdate.toISOString().slice(0, 10)}'`
+        );
+      }
+      process.exit(1);
+    })
+    .catch(err => {
+      console.log(err);
+      process.exit(1);
+    });
 });
