3660d33d2f78fdad650a73b9cfa2c4ace27410c8
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-01 16:20:15 +0000

    Complete querying famous_people

---
+++ search_person.js
@@ -0,0 +1,25 @@
+const settings = require("./settings"); // settings.json
+
+var knex = require("knex")({
+  client: "pg",
+  connection: settings
+});
+
+knex("famous_people")
+  .where({ first_name: process.argv.slice(2).toString() })
+  .then((rows) => {
+    console.log("Searching ...");
+    console.log(
+      `Found ${rows.length} person(s) by the name '${process.argv[2]}':`
+    );
+    for (const [index, row] of rows.entries()) {
+      console.log(
+        `- ${index + 1}: ${row.first_name} ${
+          row.last_name
+        }, born '${row.birthdate.toISOString().slice(0, 10)}'`
+      );
+    }
+    process.exit(1)
+  });
+
+
