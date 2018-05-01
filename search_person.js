a577f2fc1e807e0f61ded087f0aed7cd0ea8fc27
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-01 17:47:57 +0000

    Refactor search_person.js

--- search_person.js
+++ search_person.js
@@ -5,21 +5,25 @@ var knex = require("knex")({
   connection: settings
 });
 
+function handleResult(rows) {
+  console.log("Searching ...");
+  console.log(
+    `Found ${rows.length} person(s) by the name '${process.argv[2]}':`
+  );
+  for (const [index, row] of rows.entries()) {
+    console.log(
+      `- ${index + 1}: ${row.first_name} ${
+        row.last_name
+      }, born '${row.birthdate.toISOString().slice(0, 10)}'`
+    );
+  }
+  process.exit(1);
+}
+
 knex("famous_people")
   .where({ first_name: process.argv[2] })
-  .then((rows) => {
-    console.log("Searching ...");
-    console.log(
-      `Found ${rows.length} person(s) by the name '${process.argv[2]}':`
-    );
-    for (const [index, row] of rows.entries()) {
-      console.log(
-        `- ${index + 1}: ${row.first_name} ${
-          row.last_name
-        }, born '${row.birthdate.toISOString().slice(0, 10)}'`
-      );
-    }
-    process.exit(1)
+  .then(result => handleResult(result))
+  .catch(err =>{
+    console.log(err);
+    process.exit(1);
   });
-
-
