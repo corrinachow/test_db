c21e803bafd1857040994598786d3a21f7e4eb4c
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-01 17:39:43 +0000

    Refactor test_script.js

--- test_script.js
+++ test_script.js
@@ -10,34 +10,37 @@ const client = new pg.Client({
   ssl: settings.ssl
 });
 
-const query = `SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1`;
-
 [, , name] = process.argv;
+
+function handleQuery(db, searchQuery) {
+  const query = `SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1`;
+  return db.query(query, [searchQuery]);
+}
+
+function handleResult(result) {
+  console.log("Searching ...");
+  console.log(
+    `Found ${result.rows.length} person(s) by the name '${process.argv[2]}':`
+  );
+  for (const [index, row] of result.rows.entries()) {
+    console.log(
+      `- ${index + 1}: ${row.first_name} ${
+        row.last_name
+      }, born '${row.birthdate.toISOString().slice(0, 10)}'`
+    );
+  }
+  process.exit(1);
+}
 
 client.connect(err => {
   if (err) {
     return console.error("Connection Error", err);
+  } else {
+    handleQuery(client, name)
+      .then(result => handleResult(result))
+      .catch(err => {
+        console.log(err);
+        process.exit(1);
+      });
   }
-  client
-    .query(query, [name])
-    .then(result => {
-      console.log("Searching ...");
-      console.log(
-        `Found ${result.rows.length} person(s) by the name '${
-          process.argv[2]
-        }':`
-      );
-      for (const [index, row] of result.rows.entries()) {
-        console.log(
-          `- ${index + 1}: ${row.first_name} ${
-            row.last_name
-          }, born '${row.birthdate.toISOString().slice(0, 10)}'`
-        );
-      }
-      process.exit(1);
-    })
-    .catch(err => {
-      console.log(err);
-      process.exit(1);
-    });
 });
